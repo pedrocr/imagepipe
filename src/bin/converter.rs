abdf6a1bb4903d1f87e26ecde060aaf9848df832
Pedro Côrte-Real <pedro@pedrocr.net>
2021-06-02 21:18:52 +0100

    Use higher quality JPG enconding in the example

--- src/bin/converter.rs
+++ src/bin/converter.rs
@@ -66,7 +66,7 @@ fn main() {
   };
   let mut f = BufWriter::new(uf);
 
-  let mut jpg_encoder = image::jpeg::JpegEncoder::new_with_quality(&mut f, 80);
+  let mut jpg_encoder = image::jpeg::JpegEncoder::new_with_quality(&mut f, 90);
   jpg_encoder
     .encode(&decoded.data, decoded.width as u32, decoded.height as u32, ColorType::Rgb8)
     .expect("Encoding image in JPEG format failed.");
