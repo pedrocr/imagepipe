0b269304c93f96342e13119ef4e5c7c70a4802cc
Pedro Côrte-Real <pedro@pedrocr.net>
2021-06-07 10:38:52 +0100

    Implement the scaling code without the macro
    Clean up the scaling code using a Trait and generic function instead of
    the macro.

--- src/scaling.rs
+++ src/scaling.rs
@@ -29,61 +29,78 @@ fn calc_skips(idx: usize, idxmax: usize, skip: f32) -> (usize, usize, f32, f32) 
   (fromback as usize, toforward as usize, fromfactor, tofactor)
 }
 
-macro_rules! scale_down_buffer {
-  (
-    $buf:expr,
-    $nwidth:expr,
-    $nheight:expr,
-    $sum:ident,
-    $arg:expr,
-    $output_type:ty,
-    $components:literal
-  ) => {
-    {
-      let mut out = vec![0 as $output_type; $nwidth*$nheight*$components];
-      let rowskip = ($buf.width as f32) / ($nwidth as f32);
-      let colskip = ($buf.height as f32) / ($nheight as f32);
-      for (row,line) in out.chunks_exact_mut($nwidth*$components).enumerate() {
-        for col in 0..$nwidth {
-          let mut sums: [f32; 4] = [0.0;4];
-          let mut counts: [f32; 4] = [0.0;4];
-          let (fromrow, torow, topfactor, bottomfactor) = calc_skips(row, $buf.height, rowskip);
-          for y in fromrow..torow {
-            let (fromcol, tocol, leftfactor, rightfactor) = calc_skips(col, $buf.width, colskip);
-            for x in fromcol..tocol {
-              let factor = {
-                (if y == fromrow {topfactor} else if y == torow {bottomfactor} else {1.0}) *
-                (if x == fromcol {leftfactor} else if x == tocol {rightfactor} else {1.0})
-              };
+trait FromToF32 {
+    fn from_f32(val: f32) -> Self;
+    fn to_f32(&self) -> f32;
+    fn zero() -> Self;
+}
 
-              $sum(&$buf.data, &mut sums, &mut counts, x, y, $buf.width, factor, $arg);
-            }
-          }
+impl FromToF32 for u8 {
+    fn from_f32(val: f32) -> Self { val as u8 }
+    fn to_f32(&self) -> f32 { *self as f32 }
+    fn zero() -> Self { 0 }
+}
 
-          for c in 0..$components {
-            if counts[c] > 0.0 {
-              line[col*$components+c] = (sums[c] / counts[c]) as $output_type;
-            }
-          }
-        }
-      }
-      out
-    }
-  }
+impl FromToF32 for f32 {
+    fn from_f32(val: f32) -> Self { val }
+    fn to_f32(&self) -> f32 { *self }
+    fn zero() -> Self { 0.0 }
 }
 
 #[inline(always)]
-fn sum_cfa_1_f32(src: &[f32], sums: &mut [f32; 4], counts: &mut [f32; 4], x: usize, y: usize, width: usize, factor: f32, cfa: &CFA) {
-  let c = cfa.color_at(y, x);
-  sums[c] += (src[y*width+x] as f32) * factor;
-  counts[c] += factor;
+fn scale_down_buffer<T: FromToF32 + Copy>(
+  src: &[T],
+  width: usize,
+  height: usize,
+  nwidth: usize,
+  nheight: usize,
+  components: usize,
+  cfa: Option<&CFA>,
+  ) -> Vec<T> {
+  let mut out = vec![T::zero(); nwidth*nheight*components];
+  let rowskip = (width as f32) / (nwidth as f32);
+  let colskip = (height as f32) / (nheight as f32);
+  for (row,line) in out.chunks_exact_mut(nwidth*components).enumerate() {
+    for col in 0..nwidth {
+      let mut sums: [f32; 4] = [0.0;4];
+      let mut counts: [f32; 4] = [0.0;4];
+      let (fromrow, torow, topfactor, bottomfactor) = calc_skips(row, height, rowskip);
+      for y in fromrow..torow {
+        let (fromcol, tocol, leftfactor, rightfactor) = calc_skips(col, width, colskip);
+        for x in fromcol..tocol {
+          let factor = {
+            (if y == fromrow {topfactor} else if y == torow {bottomfactor} else {1.0}) *
+            (if x == fromcol {leftfactor} else if x == tocol {rightfactor} else {1.0})
+          };
+
+            if let Some(cfa) = cfa {
+              let c = cfa.color_at(y, x);
+              sums[c] += src[y*width+x].to_f32() * factor;
+              counts[c] += factor;
+           } else {
+              for c in 0..components {
+                sums[c] += src[(y*width+x)*components+c].to_f32() * factor;
+                counts[c] += factor;
+              }
+           }
+        }
+      }
+
+      for c in 0..components {
+        if counts[c] > 0.0 {
+          line[col*components+c] = T::from_f32(sums[c] / counts[c]);
+        }
+      }
+    }
+  }
+  out
 }
 
 pub fn scaled_demosaic(cfa: CFA, buf: &OpBuffer, nwidth: usize, nheight: usize) -> OpBuffer {
-  log::debug!("Doing a scaled demosaic from {}x{} to {}x{}", buf.width, buf.height, nwidth, nheight);
   assert_eq!(buf.colors, 1); // When we're in demosaic we start with a 1 color buffer
 
-  let data = scale_down_buffer!(buf, nwidth, nheight, sum_cfa_1_f32, &cfa, f32, 4);
+  log::debug!("Doing a scaled demosaic from {}x{} to {}x{}", buf.width, buf.height, nwidth, nheight);
+  let data = scale_down_buffer(&buf.data, buf.width, buf.height, nwidth, nheight, 4, Some(&cfa));
 
   OpBuffer {
     width: nwidth,
@@ -91,22 +108,14 @@ pub fn scaled_demosaic(cfa: CFA, buf: &OpBuffer, nwidth: usize, nheight: usize) 
     data,
     monochrome: buf.monochrome,
     colors: 4,
-  }
-}
-
-#[inline(always)]
-fn sum_4_f32(src: &[f32], sums: &mut [f32; 4], counts: &mut [f32; 4], x: usize, y: usize, width: usize, factor: f32, _extra: ()) {
-  for c in 0..4 {
-    sums[c] += src[(y*width+x)*4+c] as f32 * factor;
-    counts[c] += factor;
   }
 }
 
 pub fn scale_down_opbuf(buf: &OpBuffer, nwidth: usize, nheight: usize) -> OpBuffer {
-  log::debug!("Scaling OpBuffer from {}x{} to {}x{}", buf.width, buf.height, nwidth, nheight);
   assert_eq!(buf.colors, 4); // When we're scaling down we're always at 4 cpp
 
-  let data = scale_down_buffer!(buf, nwidth, nheight, sum_4_f32, (), f32, 4);
+  log::debug!("Scaling OpBuffer from {}x{} to {}x{}", buf.width, buf.height, nwidth, nheight);
+  let data = scale_down_buffer(&buf.data, buf.width, buf.height, nwidth, nheight, 4, None);
 
   OpBuffer {
     width: nwidth,
@@ -114,20 +123,12 @@ pub fn scale_down_opbuf(buf: &OpBuffer, nwidth: usize, nheight: usize) -> OpBuff
     data,
     monochrome: buf.monochrome,
     colors: 4,
-  }
-}
-
-#[inline(always)]
-fn sum_3_u8(src: &[u8], sums: &mut [f32; 4], counts: &mut [f32; 4], x: usize, y: usize, width: usize, factor: f32, _extra: ()) {
-  for c in 0..3 {
-    sums[c] += src[(y*width+x)*3+c] as f32 * factor;
-    counts[c] += factor;
   }
 }
 
 pub fn scale_down_srgb(buf: &SRGBImage, nwidth: usize, nheight: usize) -> SRGBImage {
   log::debug!("Scaling SRGBImage from {}x{} to {}x{}", buf.width, buf.height, nwidth, nheight);
-  let data = scale_down_buffer!(buf, nwidth, nheight, sum_3_u8, (), u8, 3);
+  let data = scale_down_buffer(&buf.data, buf.width, buf.height, nwidth, nheight, 3, None);
 
   SRGBImage {
     width: nwidth,
